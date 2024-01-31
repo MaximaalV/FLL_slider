e1afd7c3f8a01c2486cecf7df26afaa4526e0af9
Max de Vocht <maxdevocht@hotmail.com>
2024-01-31 11:03:56 +0100

    Updated js

---
+++ script.js
@@ -0,0 +1,25 @@
+var splide = new Splide( '.splide', {
+    perPage: 3,
+    perMove: 1,
+    gap    : '1.5rem',
+    padding: '3rem',
+    type: 'loop',
+    drag: 'free',
+    snap: true,
+    autoplay: true,
+    breakpoints: {
+      640: {
+        perPage: 2,
+        gap    : '.7rem',
+        padding: '1.5rem',
+      },
+      480: {
+        perPage: 1,
+        gap    : '.7rem',
+        padding: '1.5rem',
+      },
+    },
+} );
+  
+splide.mount();
+  
